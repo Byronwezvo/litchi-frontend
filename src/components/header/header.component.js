bc0cd721d65dd1ad65484e388f4a2d0a9bda607f
Byron Wezvo <byronwezvo@outlook.com>
2020-07-28 17:02:32 +0200

    Added new links to the header

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -23,18 +23,28 @@ class Header extends React.Component {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div
-          className="collapse navbar-collapse justify-content-center"
+          className="collapse navbar-collapse justify-content-end"
           id="navbarNav"
         >
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Company Admin Log In
+              <Link className="nav-link" to="/features">
+                Features
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/add-user">
-                Company Client Log in
+              <Link className="nav-link" to="/pricing">
+                Pricing
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                log in
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/signup">
+                Sign up
               </Link>
             </li>
           </ul>
