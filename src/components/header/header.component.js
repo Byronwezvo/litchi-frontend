bcfd0021e8d9b1f93d5c39ff5cc716198a1841f7
Byron Wezvo <byronwezvo@outlook.com>
2020-07-28 13:56:33 +0200

    Modified header component

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,8 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './header.css';
 
 class Header extends React.Component {
   render() {
-    return <h1>Header</h1>;
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <Link className="navbar-brand" to="/">
+          Litchi
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Company Admin Log In
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/add-user">
+                Company Client Log in
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
