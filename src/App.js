fff8d9b1a8a68464b713698bd8fa69f5ddb5e8d2
Byron Wezvo <byronwezvo@outlook.com>
2020-07-28 19:15:45 +0200

    Added Routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 
 import Header from './components/header/header.component';
 import Homepage from './components/homepage/homepage.component';
+import Features from './components/features/features.component';
+import Pricing from './components/pricing/pricing.component';
+import Login from './components/login/login.component';
+import Signup from './components/signup/signup.component';
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Header />
       <br />
       <Route path="/" exact component={Homepage}></Route>
+      <Route path="/features" exact component={Features}></Route>
+      <Route path="/pricing" exact component={Pricing}></Route>
+      <Route path="/login" exact component={Login}></Route>
+      <Route path="/signup" exact component={Signup}></Route>
     </Router>
   );
 }
