bb09c60e2c6a029988e9f4da3fbe6576269e67d0
Byron Wezvo <byronwezvo@outlook.com>
2020-07-31 04:11:20 +0200

    Added new navbar fron antd
    Meh I dont love complex it is hahah or im lazy 🤣🤣🤣

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,41 +1,55 @@
 import React from 'react';
+import { Layout, Menu, PageHeader } from 'antd';
 
-import './header.css';
-import '../navbar_item/navbar_item.component';
-import NavBarItem from '../navbar_item/navbar_item.component';
-import NavBarBrand from '../navbar_brand/navbar_brand.component';
+// import './header.css';
+// import '../navbar_item/navbar_item.component';
+// import NavBarItem from '../navbar_item/navbar_item.component';
+// import NavBarBrand from '../navbar_brand/navbar_brand.component';
 
-class Header extends React.Component {
+const { Header } = Layout;
+
+class DefaultHeader extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <NavBarBrand name="Litchi" />
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="collapse navbar-collapse justify-content-end"
-          id="navbarNav"
-        >
-          <ul className="navbar-nav">
-            <NavBarItem name="Home" route="/" />
-            <NavBarItem name="Features" route="/features" />
-            <NavBarItem name="Pricing" route="/pricing" />
-            <NavBarItem name="Login" route="/login" />
-            <NavBarItem name="Signup" route="/signup" />
-          </ul>
-        </div>
-      </nav>
+      <div>
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+      </div>
+
+      // <nav className="navbar navbar-expand-lg navbar-light">
+      //   <NavBarBrand name="Litchi" />
+      //   <button
+      //     className="navbar-toggler"
+      //     type="button"
+      //     data-toggle="collapse"
+      //     data-target="#navbarNav"
+      //     aria-controls="navbarNav"
+      //     aria-expanded="false"
+      //     aria-label="Toggle navigation"
+      //   >
+      //     <span className="navbar-toggler-icon"></span>
+      //   </button>
+      //   <div
+      //     className="collapse navbar-collapse justify-content-end"
+      //     id="navbarNav"
+      //   >
+      //     <ul className="navbar-nav">
+      //       <NavBarItem name="Home" route="/" />
+      //       <NavBarItem name="Features" route="/features" />
+      //       <NavBarItem name="Pricing" route="/pricing" />
+      //       <NavBarItem name="Login" route="/login" />
+      //       <NavBarItem name="Signup" route="/signup" />
+      //     </ul>
+      //   </div>
+      // </nav>
     );
   }
 }
 
-export default Header;
+export default DefaultHeader;
