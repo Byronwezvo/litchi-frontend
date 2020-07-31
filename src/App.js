e0ef5961d5863a4aeaa7291a072728b824652ddd
Byron Wezvo <byronwezvo@outlook.com>
2020-07-31 07:12:40 +0200

    Returned old ugly view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './components/header/header.component';
 import Homepage from './screens/homepage/homepage.component';
--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,38 +1,38 @@
 import React from 'react';
 
 import './header.css';
+import NavBarBrand from '../navbar_brand/navbar_brand.component';
+import NavBarItem from '../navbar_item/navbar_item.component';
 
 class DefaultHeader extends React.Component {
   render() {
     return (
-      <div>header</div>
-
-      // <nav className="navbar navbar-expand-lg navbar-light">
-      //   <NavBarBrand name="Litchi" />
-      //   <button
-      //     className="navbar-toggler"
-      //     type="button"
-      //     data-toggle="collapse"
-      //     data-target="#navbarNav"
-      //     aria-controls="navbarNav"
-      //     aria-expanded="false"
-      //     aria-label="Toggle navigation"
-      //   >
-      //     <span className="navbar-toggler-icon"></span>
-      //   </button>
-      //   <div
-      //     className="collapse navbar-collapse justify-content-end"
-      //     id="navbarNav"
-      //   >
-      //     <ul className="navbar-nav">
-      //       <NavBarItem name="Home" route="/" />
-      //       <NavBarItem name="Features" route="/features" />
-      //       <NavBarItem name="Pricing" route="/pricing" />
-      //       <NavBarItem name="Login" route="/login" />
-      //       <NavBarItem name="Signup" route="/signup" />
-      //     </ul>
-      //   </div>
-      // </nav>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <NavBarBrand name="Litchi" />
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            <NavBarItem name="Home" route="/" />
+            <NavBarItem name="Features" route="/features" />
+            <NavBarItem name="Pricing" route="/pricing" />
+            <NavBarItem name="Login" route="/login" />
+            <NavBarItem name="Signup" route="/signup" />
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
