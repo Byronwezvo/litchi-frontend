8e0d8b9985cdc57e5987f760fa3fcc0ef7204ccd
Byron Wezvo <byronwezvo@outlook.com>
2020-08-02 06:41:35 +0200

    New defalut font and color
    Picked Blue from red

--- src/components/navbar_item/navbar_item.component.js
+++ src/components/navbar_item/navbar_item.component.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './navbar_item.css';
 
 function NavBarItem(props) {
   return (
     <li className="nav-item">
       <Link className="nav-link" to={props.route}>
-        <div className="h6">{props.name}</div>
+        <div className="navbar-item-text">{props.name}</div>
       </Link>
     </li>
   );
