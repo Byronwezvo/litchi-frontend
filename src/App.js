4f9585db77c45f0a6cdd8d9be88ead3048459077
Byron Wezvo <byronwezvo@outlook.com>
2020-08-04 12:30:46 +0200

    Added dashboard route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Features from './screens/features/features.screen';
 import Pricing from './screens/pricing/pricing.screen';
 import Login from './screens/login/login.screen';
 import Signup from './screens/signup/signup.screen';
+import Dashboard from './screens/dashboard/dashboard.screen';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <Route path="/pricing" exact component={Pricing}></Route>
       <Route path="/login" exact component={Login}></Route>
       <Route path="/signup" exact component={Signup}></Route>
+      <Route path="/dashboard" exact component={Dashboard}></Route>
     </Router>
   );
 }
