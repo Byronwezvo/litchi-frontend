54e4afa69bd1903653aeee6f2d220af629b57f19
Byron Wezvo <byronwezvo@outlook.com>
2020-08-04 17:12:43 +0200

    Added method to avoid user from accessing dashboard
    with no log in you dum animal

--- src/screens/dashboard/dashboard.screen.js
+++ src/screens/dashboard/dashboard.screen.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 class Dashboard extends React.Component {
   componentDidMount() {
-    console.log('User is not Authenticated');
+    // -> Try find data stored in Session storage
+    const payload = sessionStorage.getItem('payload');
+
+    // -> If null which means user in not authenticated do this
+    if (payload === null) {
+      // -> route user to login screen
+      this.routeToLogin();
+    } else {
+      console.log('Yey');
+    }
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.routeToLogin = this.routeToLogin.bind(this);
+  }
+
+  /**
+   * # Route user to log in
+   *
+   * This function is called when a user automatically tries to access
+   * dashboard with no login
+   *
+   * @author Byron Wezvo
+   */
+  routeToLogin() {
+    window.location = '/login';
   }
 
   render() {
