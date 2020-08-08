09a2c5e6eda8dad8956808c54815d1e36ee58ff3
Byron Wezvo <byronwezvo@outlook.com>
2020-08-08 09:27:11 +0200

    Added dashboard side drawer componnt

---
+++ src/components/dashboard_sidedrawer/dashboard_sidedrawer.component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './dashboard_sidedrawer.css';
+
+function DashboardSidedrawer() {
+  return <div className="col-sm-2 side-drawer">working</div>;
+}
+
+export default DashboardSidedrawer;
