8f602e527e03cd3b077c6ba819e7b7daebdceaec
Byron Wezvo <byronwezvo@outlook.com>
2020-08-08 15:30:43 +0200

    Created new components [needs clean up]

--- src/dashboard_views/home_view/home.view.js
+++ src/dashboard_views/home_view/home.view.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
+import './homeview.css';
+
 class Home extends React.Component {
   render() {
     return (
       <div className="col-sm-10 overflow-auto">
-        <div class="d-md-flex">
-          <div class="overflow-auto p-3 mb-3 mb-md-0 mr-md-3 bg-light">
-            {/* Add design items here */}
+        {/* TODO : move this code to a component */}
+        <div class="overflow-auto p-3 mb-3 mb-md-0 mr-md-3">
+          <div class="container">
+            <div class="row">
+              {/* TODO : move this code to a component */}
+              <div class="col-sm">
+                <div className="dashboard-card">
+                  <div className="dashboard-card-heading text-center">
+                    <span className="dashboard-card-icon">
+                      <i class="icofont-envelope"></i>
+                    </span>
+                    New Applications
+                  </div>
+                  <div className="text-center">28</div>
+                </div>
+              </div>
+              <div class="col-sm">One of three columns</div>
+              <div class="col-sm">One of three columns</div>
+              <div class="col-sm">One of three columns</div>
+              <div class="col-sm">One of three columns</div>
+            </div>
           </div>
         </div>
       </div>
