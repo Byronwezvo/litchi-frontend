0ef5f3b5603f145669c1fb1e6eded2d7c0786aa8
Byron Wezvo <byronwezvo@outlook.com>
2020-08-08 15:32:37 +0200

    Moved code

---
+++ src/components/home_card-section/homecard_section.component.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function HomeCardSection() {
+  return (
+    <div class="overflow-auto p-3 mb-3 mb-md-0 mr-md-3">
+      <div class="container">
+        <div class="row">
+          {/* TODO : move this code to a component */}
+          <div class="col-sm">
+            <div className="dashboard-card">
+              <div className="dashboard-card-heading text-center">
+                <span className="dashboard-card-icon">
+                  <i class="icofont-envelope"></i>
+                </span>
+                New Applications
+              </div>
+              <div className="text-center">28</div>
+            </div>
+          </div>
+          <div class="col-sm">One of three columns</div>
+          <div class="col-sm">One of three columns</div>
+          <div class="col-sm">One of three columns</div>
+          <div class="col-sm">One of three columns</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomeCardSection;
--- src/dashboard_views/home_view/home.view.js
+++ src/dashboard_views/home_view/home.view.js
@@ -1,34 +1,14 @@
 import React from 'react';
 
 import './homeview.css';
+import HomeCardSection from '../../components/home_card-section/homecard_section.component';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="col-sm-10 overflow-auto">
         {/* TODO : move this code to a component */}
-        <div class="overflow-auto p-3 mb-3 mb-md-0 mr-md-3">
-          <div class="container">
-            <div class="row">
-              {/* TODO : move this code to a component */}
-              <div class="col-sm">
-                <div className="dashboard-card">
-                  <div className="dashboard-card-heading text-center">
-                    <span className="dashboard-card-icon">
-                      <i class="icofont-envelope"></i>
-                    </span>
-                    New Applications
-                  </div>
-                  <div className="text-center">28</div>
-                </div>
-              </div>
-              <div class="col-sm">One of three columns</div>
-              <div class="col-sm">One of three columns</div>
-              <div class="col-sm">One of three columns</div>
-              <div class="col-sm">One of three columns</div>
-            </div>
-          </div>
-        </div>
+        <HomeCardSection />
       </div>
     );
   }
