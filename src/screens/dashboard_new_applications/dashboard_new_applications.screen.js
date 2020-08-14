2019ccfcc6e08479f0709c3c8d09a3e6153c231d
Byron Wezvo <byronwezvo@outlook.com>
2020-08-14 16:43:51 +0200

    Added default view elements

--- src/screens/dashboard_new_applications/dashboard_new_applications.screen.js
+++ src/screens/dashboard_new_applications/dashboard_new_applications.screen.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import DashboardHeader from '../../components/dashboard_header/dashboard_header.component';
+import DashboardSideBar from '../../components/dashboard_sidebar/dashboard_sidebar.component';
+
 class DashboardNewApplication extends React.Component {
   render() {
     return (
       <div>
-        <h1>New AApplication</h1>
+        <DashboardHeader />
+        <div className="dashboard-page-warper">
+          <div className="row">
+            <DashboardSideBar />
+          </div>
+        </div>
       </div>
     );
   }
