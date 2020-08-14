25a5a55cad0de893276789c031436288c4c2bff5
Byron Wezvo <byronwezvo@outlook.com>
2020-08-14 16:20:56 +0200

    Added new screen

---
+++ src/screens/dashboard_client_list/dashboard_client_list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class DashboardClientList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Dashboard Client List</h1>
+      </div>
+    );
+  }
+}
