4a7b86b9761424ba4a8467562ba7f1a3b3bbbc33
Byron Wezvo <byronwezvo@outlook.com>
2020-08-14 16:40:00 +0200

    Added dummy code

---
+++ src/screens/dashboard_new_applications/dashboard_new_applications.screen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DashboardNewApplication extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>New AApplication</h1>
+      </div>
+    );
+  }
+}
+
+export default DashboardNewApplication;
