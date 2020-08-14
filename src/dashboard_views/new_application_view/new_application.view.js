03a87d41c9e2e763a3d476303af599644a62ba75
Byron Wezvo <byronwezvo@outlook.com>
2020-08-14 17:12:20 +0200

    Added ugly form

--- src/dashboard_views/new_application_view/new_application.view.js
+++ src/dashboard_views/new_application_view/new_application.view.js
@@ -1,10 +1,80 @@
 import React from 'react';
+import CustomInput from '../../components/custom_input/custom_input.component';
 
 class NewApplication extends React.Component {
   render() {
     return (
       <div className="col-sm-10 overflow-auto">
-        <h1>Hi there!</h1>
+        <div className="container">
+          <form>
+            {/* Client Details */}
+            <div className="form-group row">
+              <div className="col-sm-6 mb-3 mb-sm-0">
+                <CustomInput
+                  label="Client Name"
+                  type="text"
+                  placeholder="John Doe"
+                />
+              </div>
+              <div className="col-sm-6 ">
+                <CustomInput
+                  label="Client Mobile Number"
+                  type="text"
+                  placeholder="microfinance"
+                />
+              </div>
+            </div>
+            <div className="form-group row">
+              <div className="col-sm-6 mb-3 mb-sm-0">
+                <CustomInput
+                  label="id number"
+                  type="text"
+                  placeholder="John Doe"
+                />
+              </div>
+              <div className="col-sm-6 ">
+                <CustomInput
+                  label="Address"
+                  type="text"
+                  placeholder="microfinance"
+                />
+              </div>
+            </div>
+            {/* Client Next of keen Details */}
+            <div className="form-group row">
+              <div className="col-sm-6 mb-3 mb-sm-0">
+                <CustomInput
+                  label="Client Name"
+                  type="text"
+                  placeholder="John Doe"
+                />
+              </div>
+              <div className="col-sm-6 ">
+                <CustomInput
+                  label="Client Mobile Number"
+                  type="text"
+                  placeholder="microfinance"
+                />
+              </div>
+            </div>
+            <div className="form-group row">
+              <div className="col-sm-6 mb-3 mb-sm-0">
+                <CustomInput
+                  label="id number"
+                  type="text"
+                  placeholder="John Doe"
+                />
+              </div>
+              <div className="col-sm-6 ">
+                <CustomInput
+                  label="Address"
+                  type="text"
+                  placeholder="microfinance"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
--- src/screens/dashboard_new_applications/dashboard_new_applications.screen.js
+++ src/screens/dashboard_new_applications/dashboard_new_applications.screen.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import DashboardHeader from '../../components/dashboard_header/dashboard_header.component';
 import DashboardSideBar from '../../components/dashboard_sidebar/dashboard_sidebar.component';
+import NewApplicationView from '../../dashboard_views/new_application_view/new_application.view';
 
 class DashboardNewApplication extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class DashboardNewApplication extends React.Component {
         <div className="dashboard-page-warper">
           <div className="row">
             <DashboardSideBar />
+            <NewApplicationView />
           </div>
         </div>
       </div>
